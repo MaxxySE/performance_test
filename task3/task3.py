675cbf7bd7c5b248d275fb579546b9d60013f1a8
Дидковский Максим <maximrymin15@gmail.com>
2025-11-20 01:07:18 +0400

    task 3 in progress

--- task3/task3.py
+++ task3/task3.py
@@ -7,22 +7,32 @@ def get_json(filename):
 
     return js
 
-values_json = get_json("values.json")
-tests_json = get_json("tests.json")
-
-def get_values():
+def get_values_map(values_json):
     values_map = {}
 
     for entry in values_json['values']:
         values_map[entry['id']] = entry['value']
     return values_map
 
-def put_values(values_map):
-    for entry in tests_json['tests']:
-        print(entry)
+def put_values(tests_list, values_map):
 
-put_values(get_values())
+    for test in tests_list:
+        if test['id'] in values_map:
+            test['value'] = values_map[test['id']]
 
-get_values()
-#print(get_json("values.json")['values'])
-#print(get_json("tests.json"))
+        if 'values' in test:
+            put_values(test['values'], values_map)
+
+def main():
+    values_json = get_json("values.json")
+    tests_json = get_json("tests.json")
+
+    values_map = get_values_map(values_json)
+
+    if 'tests' in tests_json:
+        put_values(tests_json['tests'], values_map)
+
+
+    print(tests_json)
+
+main()
