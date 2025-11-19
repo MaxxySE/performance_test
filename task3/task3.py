32fddb4c4f86476387769732a21bf38e4378845a
Дидковский Максим <maximrymin15@gmail.com>
2025-11-20 01:41:09 +0400

    task 3 completed

--- task3/task3.py
+++ task3/task3.py
@@ -1,5 +1,5 @@
 import json
-
+import sys
 
 def get_json(filename):
     with open(filename) as f:
@@ -24,15 +24,27 @@ def put_values(tests_list, values_map):
             put_values(test['values'], values_map)
 
 def main():
-    values_json = get_json("values.json")
-    tests_json = get_json("tests.json")
+
+    if len(sys.argv) < 4:
+        print("Ошибка! Нужно передать 3 аргумента: values.json tests.json report.json")
+        return
+
+    path_values = sys.argv[1]
+    path_tests = sys.argv[2]
+    path_report = sys.argv[3]
+
+    values_json = get_json(path_values)
+    tests_json = get_json(path_tests)
 
     values_map = get_values_map(values_json)
 
     if 'tests' in tests_json:
         put_values(tests_json['tests'], values_map)
 
-
     print(tests_json)
+
+    with open(path_report, "w", encoding='utf-8') as f:
+        json.dump(tests_json, f, indent=2, ensure_ascii=False)
+    print(f"Результат успешно записан в файл: {path_report}")
 
 main()
